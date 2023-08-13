42dc9837b7d0bd9e86152cbd0e5cbec739cb8e8e
Andrew <zeyu.li23@gmail.com>
2023-08-13 12:40:45 -0600

    fix: the right link this time

--- src/components/constants/constants.tsx
+++ src/components/constants/constants.tsx
@@ -1,5 +1,5 @@
 const DEFAULT_TITLE = "PlayDate";
-const DEFAULT_DESCRIPTION = "Dating done easy!";
+const DEFAULT_DESCRIPTION = "Dating done easy! Try PlayDate today for free!";
 const DEFAULT_BASE_URL = "https://playdate.uwulang.com/";
 
 const CONSTANTS = {
@@ -15,7 +15,7 @@ const CONSTANTS = {
   ],
   DEFAULT_SITE_NAME: "PlayDate",
   DEFAULT_BASE_URL: DEFAULT_BASE_URL,
-  DEFAULT_PREVIEW_IMG: "https://playdate.uwulang.com/ogimg.png",
+  DEFAULT_PREVIEW_IMG: "https://playdate.uwulang.com/ogimage.png",
   DEFAULT_TWITTER_TITLE: DEFAULT_TITLE,
   DEFAULT_TWITTER_DESCRIPTION: DEFAULT_DESCRIPTION,
   DEFAULT_GOOGLE_ANALYTICS: "G-KLVV6HWCRZ",
